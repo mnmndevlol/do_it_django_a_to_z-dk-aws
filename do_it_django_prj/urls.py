f086964c7fe237633e82656f7397393dbb3a26a0
mnmndevlol <mnmndevlol@gmail.com>
2021-07-03 12:53:15 +0900

    Nginx 적용하기 까지. (596p)

---
+++ do_it_django_prj/urls.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+
+    path('blog/', include('blog.urls')),
+    path('admin/', admin.site.urls),
+    path('markdownx/', include('markdownx.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('', include('single_pages.urls')),
+
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
